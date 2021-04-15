8ad54690e2a69930964e48a92eb65d93e54ea229
Lucas Cunha <lucasgb7_ks@hotmail.com>
2021-04-15 17:11:36 -0300

    convertendo calculos para 64 bits

--- Random/counterBasedRNG.py
+++ Random/counterBasedRNG.py
@@ -1,27 +1,35 @@
 import time
+import numpy as np
 
 def squares(ctr, key):
     y = x = ctr * key
+    print("y = x = ctr * key:", hex(y))
     z = y + key
-    x = x * x + y; x = (x >> 32) | (x << 32)
-    x = x * x + z; x = (x >> 32) | (x << 32)
-    x = x * x + y; x = (x >> 32) | (x << 32)
+    print("z = y + key:", hex(z))
+    # Circular Shift
+    two5 = np.uint64(32) # 2^5
+    x = x * x + y; x = (x >> two5) | (x << two5)
+    print("1º Round:", hex(x))
+    x = x * x + z; x = (x >> two5) | (x << two5)
+    print("2º Round:", hex(x))
+    x = x * x + y; x = (x >> two5) | (x << two5)
+    print("3º Round:", hex(x))
 
-    return (x*x + z) >> 32
+    print("4º Round:", hex((x*x + z) >> two5))
+    return (x*x + z) >> two5    
 
 if __name__ == "__main__":
-    key = 0xfb9e125878fa6cb3
-    two32 = 4294967296
-    n = 100000
-    sum = 0
-    print("Key: ", key)
-    print("2^32: ", two32)
+    np.seterr(all='ignore') # ignora erros de overflow, divisao/zero, underflow, etc...
+    key = np.uint64(0xfb9e125878fa6cb3)
+    n = np.uint64(2)
+    sum = np.uint64(0)
 
     start = time.time()
     for i in range(n):
-        #print("\nValor: ", squares(i, key))
-        sum += (squares(i, key) // two32)
+        print("-------------------- i =", i, "--------------------")
+        sum += squares(np.uint64(i), key)
 
     end = time.time()
-    print("\nMedia: ", sum//n)
-    print("\nTempo: ", end - start)
+    print("================= RESULTADOS =================")
+    print("Media: ", hex(sum//n))
+    print("Tempo de simulacao: ", end - start)
