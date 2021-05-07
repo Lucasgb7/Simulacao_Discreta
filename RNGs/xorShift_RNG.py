3cd642f1c5c73cd0243c6720ad77c6012d325280
Lucas Cunha <lucasgb7_ks@hotmail.com>
2021-05-07 14:54:40 -0300

    xorShift x2

--- RNGs/xorShift_RNG.py
+++ RNGs/xorShift_RNG.py
@@ -1,24 +1,39 @@
 import time
 import numpy as np
+import qi2
 
-def xorShift(y, a, b, c):
-    y ^= np.uint32(y << a)
-    y ^= np.uint32(y >> b)
-    y ^= np.uint32(y << c)
+def xorShift(y):
+    y ^= np.uint32(y << 13)
+    y ^= np.uint32(y >> 17)
+    y ^= np.uint32(y << 15)
 
     return y
 
 if __name__ == "__main__":
     np.seterr(all='ignore')
-    seed = 2463534242
-    a, b, c = 13, 17, 15
-    iteracoes = 1000
+    seed = 2464581242
     y = np.uint32(seed)
+    #a, b, c = 13, 17, 15
+    #iteracoes = 1000
 
-    start = time.time()
-    for i in range(iteracoes):
-        y = xorShift(y, a, b, c)
-        print("y(", i, ") = ", y)
+    n = int(input("Número de iterações (n): "))
+    k = int(input("Número de categorias (k): "))
+    Fe = n / k       
+    results = []
+    #start = time.time()
+    for i in range(n):
+        y = (xorShift(y))
+        aux = y / 4294967295        # normaliza resultado
+        #print("Valor: ", aux)
+        #print("y(", i, ") = ", aux)
+        results.append(aux)
          
-    end = time.time()
-    print("Tempo de simulacao: ", end - start)
+    #end = time.time()
+    x2 = qi2.qi2Test(k, n, results)
+
+
+    print("================= RESULTADOS =================")
+    #print("Tempo de simulacao: ", end - start)
+    print("X²: ", x2)
+    print("Graus de Liberdade (GL):", k - 1)
+    print("Significância: 0.05")
