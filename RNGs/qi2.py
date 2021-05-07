4db6fcccbe51d864be561e7fe336eaf1b54fcd55
Lucas Cunha <lucasgb7_ks@hotmail.com>
2021-05-07 14:53:56 -0300

    inserindo funcao x2 em um arquivo

--- RNGs/qi2.py
+++ RNGs/qi2.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-def qi2Test(k, n, results):
+def qi2Test(gl, n, results):
 
-    Fe = n / k # frequencia esperada
-    intervals = np.arange(0, 1, Fe/n, dtype=float)  # define intervalos
-    frequency = np.zeros(k)    # armazena a frequencia de cada valor
+    Fe = n / gl     # frequencia esperada
+    intervals = np.arange(0, 1, Fe/n, dtype=float)   # define intervalos
+    frequency = np.zeros(gl) 
+    
     
     for value in results:
         for i in range(len(intervals)):
@@ -19,7 +20,7 @@ def qi2Test(k, n, results):
     print("Frequencia: ", frequency)
     
     x2 = 0
-    for i in range(k):
+    for i in range(gl):
         x2 += ((frequency[i] - Fe)**2)/Fe
         
     return x2
